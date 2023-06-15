5cdbb2e8c3c9fe6248a203dc7b1cab4f45578d17
teiysha <132546302+teiysha@users.noreply.github.com>
2023-06-15 08:47:21 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(-25)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("POWER")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(25)
+})
+radio.setGroup(1)
